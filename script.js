62a8a0b1f4fe8358547d601d0e831da7e331a500
bokwang <chdl2353@naver.com>
2021-01-12 22:25:46 +0900

    hover and click event

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const imgs = document.querySelectorAll("img");
 
 imgs.forEach((img) => {
+  img.addEventListener("mouseover", (e) => {
+    if (!e.target.classList.contains("focus")) {
+      e.target.style.cssText = "filter: brightness(40%) blur(0.2em);";
+    }
+  });
+
+  img.addEventListener("mouseout", (e) => {
+    e.target.style.cssText = "";
+  });
+
   img.addEventListener("click", (e) => {
-    e.target.style.filter = "brightness(50%)";
-    console.log(e.target.style.filter);
+    const focusing = document.querySelector(".focus");
+    if (focusing !== null) {
+      focusing.classList.remove("focus");
+    }
+    e.target.classList.add("focus");
   });
 });
